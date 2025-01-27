d3ced584693225c2da3abe188308dcf751bfee5f
monis_khan <monisykhan@gmail.com>
2025-01-27 15:42:30 +0530

    testing SMS login

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
 import TaskTable from "../components/TaskTable";
 import Navbar from "../shared/Navbar";
 
 const Home = () => {
   return (
     <>
+    <Link to={'/login'} >Login</Link>
       <Navbar />
       <TaskTable />
     </>
