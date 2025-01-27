aaf7020f273e654fd6f46d2ff06508b8b8b11a55
monis_khan <monisykhan@gmail.com>
2025-01-27 15:34:03 +0530

    testing SMS login

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,11 +1,34 @@
 import { Facebook, Google } from "@mui/icons-material";
-import { Box, Button, Stack } from "@mui/material";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { FacebookAuthProvider } from "firebase/auth";
+import {
+  Box,
+  Button,
+  CircularProgress,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import {
+  ConfirmationResult,
+  FacebookAuthProvider,
+  GoogleAuthProvider,
+  RecaptchaVerifier,
+  signInWithPhoneNumber,
+  signInWithPopup,
+} from "firebase/auth";
+import { useState } from "react";
 import loginImage from "../../assets/MHD_Login_img.jpg";
 import { auth } from "../../firebase";
 
 const LoginPage = () => {
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [otp, setOtp] = useState("");
+  const [verificationId, setVerificationId] = useState("");
+  console.log('verificationId:', verificationId)
+  const [loading, setLoading] = useState(false);
+  console.log('setLoading:', setLoading)
+  const [step, setStep] = useState(1);
+  console.log('setStep:', setStep)
+  const [user, setUser] = useState<ConfirmationResult | null>(null);
   const googleProvider = new GoogleAuthProvider();
   const facebookProvider = new FacebookAuthProvider();
 
@@ -23,7 +46,56 @@ const LoginPage = () => {
       const { user } = await signInWithPopup(auth, googleProvider);
       console.log("Google Sign-In Success:", user);
     } catch (error) {
-      console.error("Google Sign-In Error:", error.message);
+      console.error("Google Sign-In Error:", error);
+    }
+  };
+
+  const sendOtp = async () => {
+    const newPhoneNumber = phoneNumber.startsWith("+")
+    ? phoneNumber
+    : `+91${phoneNumber.trim()}`;
+
+  if (!/^\+?[1-9]\d{1,14}$/.test(newPhoneNumber)) {
+    alert("Invalid phone number format.");
+    return;
+  }
+
+    try {
+      const reCaptcha = new RecaptchaVerifier(
+        auth,
+        "recaptcha-container", // ID of the DOM element for reCAPTCHA
+        {
+          size: "invisible", // Or "normal" if you want to show it visibly
+          callback: (response: string) => {
+            console.log("reCAPTCHA verified:", response);
+          },
+        },
+     
+      );
+      reCaptcha.render();
+      console.log('reCaptcha:', reCaptcha)
+      const confirmationResult = await signInWithPhoneNumber(
+        auth,
+        newPhoneNumber,
+        reCaptcha
+      );
+      console.log('confirmationResult:', confirmationResult)
+      setVerificationId(confirmationResult.verificationId);
+      alert("OTP sent!");
+      setUser(confirmationResult);
+    } catch (error) {
+      console.error("Error sending OTP:", error);
+      alert("Failed to send OTP. Please try again.");
+    }
+  };
+
+  // Verify OTP
+  const verifyOtp = async () => {
+    try {
+      const userLoginSuccess = await user?.confirm(otp);
+      console.log("userLoginSuccess:", userLoginSuccess);
+    } catch (error) {
+      console.log("error:", error);
     }
   };
 
@@ -100,6 +172,82 @@ const LoginPage = () => {
           >
             Log in with Facebook
           </Button>
+
+          {/* ------------ SMS login-------- */}
+
+          <Box
+            sx={{
+              width: 400,
+              margin: "50px auto",
+              padding: 3,
+              boxShadow: 3,
+              borderRadius: 2,
+              textAlign: "center",
+            }}
+          >
+            <Typography variant="h5" sx={{ mb: 2 }}>
+              Phone Authentication
+            </Typography>
+
+            {step === 1 ? (
+              <>
+                <TextField
+                  label="Phone Number"
+                  variant="outlined"
+                  fullWidth
+                  sx={{ mb: 2 }}
+                  value={phoneNumber}
+                  onChange={(e) => setPhoneNumber(e.target.value)}
+                  placeholder="+91-1234 567890"
+                />
+
+                {/* <PhoneInput
+                  country={"auto"}
+                  value={phoneNumber}
+                  onChange={() => setPhoneNumber("+" + phoneNumber)}
+                /> */}
+                <Button
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                  onClick={sendOtp}
+                  disabled={loading}
+                >
+                  {loading ? (
+                    <CircularProgress size={24} color="inherit" />
+                  ) : (
+                    "Send OTP"
+                  )}
+                </Button>
+              </>
+            ) : (
+              <>
+                <TextField
+                  label="Enter OTP"
+                  variant="outlined"
+                  fullWidth
+                  sx={{ mb: 2 }}
+                  value={otp}
+                  onChange={(e) => setOtp(e.target.value)}
+                />
+                <Button
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                  onClick={verifyOtp}
+                  disabled={loading}
+                >
+                  {loading ? (
+                    <CircularProgress size={24} color="inherit" />
+                  ) : (
+                    "Verify OTP"
+                  )}
+                </Button>
+              </>
+            )}
+
+            <div id="recaptcha-container"></div>
+          </Box>
         </Stack>
       </Box>
     </Box>
